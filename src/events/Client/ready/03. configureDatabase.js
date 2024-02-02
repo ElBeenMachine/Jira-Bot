033f1561b53518f2a489de2cf3edb881fe91b13f
Ollie Beenham <ollie@beenhamow.co.uk>
2024-02-02 19:18:05 +0000

    Authentication with JIRA now complete

--- src/events/Client/ready/03. configureDatabase.js
+++ src/events/Client/ready/03. configureDatabase.js
@@ -1,10 +1,39 @@
 const client = require("#index.js");
 const sqlite = require("sqlite3").verbose();
 const fs = require("fs");
+const { exit } = require("process");
 
 client.on(__dirname.replace(/\\/g, "/").split("/").pop(), (client) => {
     // Make database directory if it doesn't exist
-    if (!fs.existsSync("./src/database")) {
-        fs.mkdirSync("./src/database");
+    if (!fs.existsSync("./db")) {
+        fs.mkdirSync("./db");
     }
+
+    // Create and connect to the sqlite database
+    client.db = new sqlite.Database("./db/database.db", (err) => {
+        if (err) {
+            console.error(`🔴 | ${err}`);
+            exit(1);
+        }
+        console.log("🟢 | Connected to the database");
+    });
+
+    /**
+     * Create tables if they don't exist
+     */
+
+    // Create the guilds table
+    client.db.run(
+        "CREATE TABLE IF NOT EXISTS guilds (id TEXT PRIMARY KEY, name TEXT UNIQUE)"
+    );
+
+    // Create the projects table
+    client.db.run(
+        "CREATE TABLE IF NOT EXISTS projects (id TEXT PRIMARY KEY, channelID TEXT NOT NULL, guild TEXT NOT NULL, FOREIGN KEY(guild) REFERENCES guilds(id))"
+    );
+
+    // Create the users table
+    client.db.run(
+        "CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, accessToken TEXT UNIQUE)"
+    );
 });
